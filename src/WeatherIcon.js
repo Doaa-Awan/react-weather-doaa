54aaaeabf37ec6d983ea289819f91a79637e4abf
Doaa-Awan <106412925+Doaa-Awan@users.noreply.github.com>
2022-09-17 11:27:50 -0400

    Change background colors

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,25 +3,25 @@ import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
 const codeMapping = {
-    "01d": "CLEAR_DAY",
-    "01n": "CLEAR_NIGHT",
-    "02d": "PARTLY_CLOUDY_DAY",
-    "02n": "PARTLY_CLOUDY_NIGHT",
-    "03d": "CLOUDY",
-    "03n": "CLOUDY",
-    "04d": "CLOUDY",
-    "04n": "CLOUDY",
-    "09d": "RAIN",
-    "09n": "RAIN",
-    "010d": "RAIN",
-    "010n": "RAIN",
-    "011d": "SLEET",
-    "011n": "SLEET",
-    "013d": "SNOW",
-    "013n": "SNOW",
-    "50d": "FOG",
-    "50n": "FOG"
-}
+  "01d": "CLEAR_DAY",
+  "01n": "CLEAR_DAY",
+  "02d": "PARTLY_CLOUDY_DAY",
+  "02n": "PARTLY_CLOUDY_DAY",
+  "03d": "CLOUDY",
+  "03n": "CLOUDY",
+  "04d": "CLOUDY",
+  "04n": "CLOUDY",
+  "09d": "RAIN",
+  "09n": "RAIN",
+  "010d": "RAIN",
+  "010n": "RAIN",
+  "011d": "SLEET",
+  "011n": "SLEET",
+  "013d": "SNOW",
+  "013n": "SNOW",
+  "50d": "FOG",
+  "50n": "FOG",
+};
   return (
     <span className="icon">
       <ReactAnimatedWeather
